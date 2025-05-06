240b12a74478ce7e1d734bc785c085454f08c386
orez <ihyeyeong97@gmail.com>
2025-05-06 15:13:54 +0900

    8장 연습문제 4번

---
+++ kotlin-demo/src/main/kotlin/prime_access/PrimeAccess.kt
@@ -0,0 +1,50 @@
+package prime_access
+
+import kotlin.random.Random
+import kotlin.system.measureTimeMillis
+
+class PrimeAccessRepository(
+    private val primeAccessList: PrimeAccessList
+) {
+    private val entries = primeAccessList.entries.associateBy { it.userId }
+    fun isOnAllowList(userId: String): Boolean = entries[userId]?.allowList ?: false
+    fun isOnDenyList(userId: String): Boolean = entries[userId]?.denyList ?: false
+}
+
+class PrimeAccessList(
+    val entries: List<PrimeAccessEntry>
+)
+
+class PrimeAccessEntry(
+    val userId: String,
+    val allowList: Boolean,
+    val denyList: Boolean,
+)
+
+fun main() {
+    val entries = List(200_000) {
+        PrimeAccessEntry(
+            userId = it.toString(),
+            allowList = Random.nextBoolean(),
+            denyList = Random.nextBoolean()
+        )
+    }.shuffled()
+    val accessList = PrimeAccessList(entries)
+
+    val repo: PrimeAccessRepository
+    measureTimeMillis {
+        repo = PrimeAccessRepository(accessList)
+    }.also { println("Class creation took $it ms") }
+
+    measureTimeMillis {
+        for (userId in 1L..10_000L) {
+            repo.isOnAllowList(userId.toString())
+        }
+    }.also { println("Operation took $it ms") }
+
+    measureTimeMillis {
+        for (userId in 1L..10_000L) {
+            repo.isOnDenyList(userId.toString())
+        }
+    }.also { println("Operation took $it ms") }
+}
